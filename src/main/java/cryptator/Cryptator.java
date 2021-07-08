70712ebcc632647e9ec087619f72cb0ed139da19
Arnaud Malapert <arnaud.malapert@univ-cotedazur.fr>
2021-07-08 15:54:52 +0200

    commit some draft for interfaces and classes

---
+++ src/main/java/cryptator/Cryptator.java
@@ -0,0 +1,21 @@
+/**
+ * This file is part of cryptator, https://github.com/arnaud-m/cryptator
+ *
+ * Copyright (c) 2021, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator;
+
+public class Cryptator {
+
+	public Cryptator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/main/java/cryptator/specs/ICryptaSolution.java
@@ -0,0 +1,15 @@
+/**
+ * This file is part of cryptator, https://github.com/arnaud-m/cryptator
+ *
+ * Copyright (c) 2021, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator.specs;
+
+public interface ICryptaSolution {
+
+	int getValue(char symbol) throws IllegalArgumentException;
+
+}
