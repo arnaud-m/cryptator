30e1b854be4b44e29e8c95e9bb3791b35f8a052f
Arnaud Malapert <arnaud.malapert@univ-cotedazur.fr>
2021-08-03 15:09:45 +0200

    handle user decision model + logging #8

--- src/main/java/cryptator/Cryptamancer.java
+++ src/main/java/cryptator/Cryptamancer.java
@@ -8,31 +8,65 @@
  */
 package cryptator;
 
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import cryptator.game.CryptaGameEngine;
+import cryptator.game.CryptaGameException;
 import cryptator.parser.CryptaParserWrapper;
 import cryptator.solver.CryptaModeler;
+import cryptator.specs.ICryptaGameEngine;
 import cryptator.specs.ICryptaNode;
 
 public class Cryptamancer {
 
-	public Cryptamancer() {}
+	private static final Logger LOGGER = Logger.getLogger(Cryptamancer.class.getName());
+
+
+
+	private static Boolean parseDecision(Scanner s, ICryptaGameEngine engine) throws CryptaGameException {
+		//s.nextLine();
+		if(s.hasNext()) {
+			final String symbol = s.next();
+			System.out.println(symbol);
+			if(symbol.length() == 1 && s.hasNext()) {
+				final CryptaOperator operator =  CryptaOperator.valueOfToken(s.next());
+				if(s.hasNextInt()) {
+					final int value = s.nextInt();
+					return engine.takeDecision(symbol.charAt(0), operator, value);
+				}
+			}
+		}
+		return null;
+	}
+
 
 	public static void main(String[] args) throws Exception {
+		final Scanner scanner = new Scanner(System.in);
 		final CryptaParserWrapper parser = new CryptaParserWrapper();
-    	final ICryptaNode node = parser.parse("send+more=money");
+		final ICryptaNode node = parser.parse("send+more=money");
 		final CryptaConfig config = new CryptaConfig();
-		config.setSolutionLimit(1);
 		final CryptaModeler modeler= new CryptaModeler();
+		// TODO Log Config ?
 		CryptaGameEngine engine = new CryptaGameEngine(modeler.model(node, config));
 		engine.setUp();
-		System.out.println(engine.takeDecision('s', CryptaOperator.EQ, 9));
-		System.out.println(engine.takeDecision('o', CryptaOperator.EQ, 1));
-		System.out.println(engine.takeDecision('r', CryptaOperator.GEQ, 4));
-		System.out.println(engine.takeDecision('y', CryptaOperator.GT, 4));
-		System.out.println(engine.takeDecision('y', CryptaOperator.GT, 6));
-		
+		int n = 1;
+		while(n < 5) {
+			LOGGER.log(Level.INFO, "Turn {0} : enter a decision:", n);
+			try {
+				Boolean answer = parseDecision(scanner, engine);
+				if(answer == null) 	LOGGER.warning("Cannot parse the decision.");
+				else if (answer.booleanValue()) LOGGER.info("decision accepted.");
+				else LOGGER.info("decision rejected.");
+			} catch (CryptaGameException e) {
+				LOGGER.log(Level.WARNING, "failure while taking the decision.", e);
+			}
+			n++;
+		}
 		engine.tearDown();
-		
+
 	}
 
 }
--- src/main/java/cryptator/game/CryptaGameEngine.java
+++ src/main/java/cryptator/game/CryptaGameEngine.java
@@ -1,7 +1,17 @@
+/**
+ * This file is part of cryptator, https://github.com/arnaud-m/cryptator
+ *
+ * Copyright (c) 2021, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
 package cryptator.game;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
@@ -15,6 +25,8 @@ import cryptator.solver.CryptaModel;
 import cryptator.specs.ICryptaGameEngine;
 
 public class CryptaGameEngine implements ICryptaGameEngine {
+
+	private static final Logger LOGGER = Logger.getLogger(CryptaGameEngine.class.getName());
 
 	public final CryptaModel gameModel;
 
@@ -37,57 +49,75 @@ public class CryptaGameEngine implements ICryptaGameEngine {
 	public void setUp() throws CryptaGameException {
 		Solver solver = gameModel.getModel().getSolver();
 		if( ! solver.solve() ) throw new CryptaGameException("Cryptarithm has no solution.");
-		System.out.println(gameModel.recordSolution());
+		LOGGER.log(Level.INFO, "display the initial cryptarithm solution:\n{0}", gameModel.recordSolution());
 	}
 
-	private final Constraint makeDecision(char symbol, CryptaOperator reOperator, int value) throws CryptaGameException {
-		final IntVar var = gameModel.getMap().get(symbol);
+	private final static Constraint makeDecision(CryptaModel model, char symbol, CryptaOperator reOperator, int value) throws CryptaGameException {
+		final IntVar var = model.getMap().get(symbol);
 		if(var == null) throw new CryptaGameException("Cannot find variable for symbol: " + symbol);
-		final IntVar val = gameModel.getModel().intVar(value);
+		final IntVar val = model.getModel().intVar(value);
 		return ( (ReExpression) reOperator.getExpression().apply(var, val)).decompose();
 	}
-	
-	private final boolean probeDecision(Constraint decision) {
-		final Model m = gameModel.getModel();
+
+	private final void propagate(CryptaModel model, Constraint decision) throws ContradictionException {
+		final Model m = model.getModel();
 		m.post(decision);
-		
+		LOGGER.log(Level.INFO, "propagate decision {0} in model {1}", new Object[] {decision, m.getName()});
+		m.getSolver().propagate();
+	}
+
+	private final boolean probeGameDecision(Constraint decision) {
+//		final Model m = gameModel.getModel();
+//		m.post(decision);	
 //		m.getSolver().reset();
 //		return m.getSolver().solve();
 
 		try {
-			m.getSolver().propagate();
-			// System.out.println("\nPROPAGATE");
+			propagate(gameModel, decision);
 			return true;
 		} catch (ContradictionException e) {
-			return m.getSolver().solve();
+			return gameModel.getModel().getSolver().solve();
 		}
 	}
+	
+	private final void propagateUserDecision(Constraint decision) throws CryptaGameException {
+		try {
+			propagate(decisionModel, decision);
+			LOGGER.log(Level.INFO, "{0}", decisionModel);
+		} catch (ContradictionException e) {
+			throw new CryptaGameException("decision solver should not fail !");
+		}
+		
+	}
 
-	private final void refuteDecision(Constraint decision) throws CryptaGameException {
+	private final void refuteGameDecision(Constraint decision) throws CryptaGameException {
 		final Model m = gameModel.getModel();
 		m.unpost(decision);
 		m.post(decision.getOpposite());
 		// FIXME m.getSolver().restart();
 		m.getSolver().reset();
 		if( ! m.getSolver().solve()) throw new CryptaGameException("Cannot refute decision" + decision);
+		
 	}
 
 	@Override
 	public boolean takeDecision(char symbol, CryptaOperator reOperator, int value) throws CryptaGameException {
+		LOGGER.log(Level.INFO, "Take decision: {0} {1} {2}.", new Object[] {symbol, reOperator, value});
 		final IntVar var = gameModel.getMap().get(symbol);
 		if(var == null) throw new CryptaGameException("Cannot find variable for symbol: " + symbol);
-		final Constraint dec = makeDecision(symbol, reOperator, value);
-		if(probeDecision(dec)) {
-			// System.out.println("\nPROBE " + dec + "\n" + gameModel.recordSolution());
+		final Constraint gdec = makeDecision(gameModel, symbol, reOperator, value);
+		final Constraint ddec = makeDecision(decisionModel, symbol, reOperator, value);
+		if(probeGameDecision(gdec)) {
+			propagateUserDecision(ddec);
 			return true;
-		}
-		else {
-			refuteDecision(dec);
-			// System.out.println("\nREFUTE " + dec + "\n" + gameModel.recordSolution());
+		} else {
+			refuteGameDecision(gdec);
+			propagateUserDecision(ddec.getOpposite());
 			return false;
 		}
 	}
-
+	
+	
 	public void tearDown() {
 
 	}
