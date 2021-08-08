6fd26d9ae340c851ccd09af924d30a7323403d4a
Arnaud Malapert <arnaud.malapert@univ-cotedazur.fr>
2021-08-08 15:13:41 +0200

    move methods used only for testing #6

--- src/test/java/cryptator/GTSolverTest.java
+++ src/test/java/cryptator/GTSolverTest.java
@@ -8,8 +8,6 @@
  */
 package cryptator;
 
-import static cryptator.solver.gentest.GTSolverUtils.arrayVarToString;
-import static cryptator.solver.gentest.GTSolverUtils.makeArray;
 import static cryptator.tree.TreeUtils.writeInorder;
 import static cryptator.tree.TreeUtils.writePostorder;
 import static cryptator.tree.TreeUtils.writePreorder;
@@ -31,12 +29,13 @@ import org.junit.Test;
 import cryptator.parser.CryptaParserException;
 import cryptator.parser.CryptaParserWrapper;
 import cryptator.solver.gentest.CryptaGTModel;
-import cryptator.solver.gentest.CryptaGTSolver;
 import cryptator.solver.gentest.CryptaGTSolution;
+import cryptator.solver.gentest.CryptaGTSolver;
 import cryptator.solver.gentest.GTVariable;
 import cryptator.specs.ICryptaEvaluation;
 import cryptator.specs.ICryptaNode;
 import cryptator.tree.CryptaEvaluation;
+import cryptator.tree.CryptaEvaluationException;
 import cryptator.tree.CryptaLeaf;
 import cryptator.tree.CryptaNode;
 import cryptator.tree.GraphizExporter;
@@ -232,26 +231,26 @@ public class GTSolverTest {
 		long start = System.currentTimeMillis();
 		//algo incremental
 		CryptaGTSolver GTSolver= new CryptaGTSolver(ABC);
-		ArrayList<Integer> tab2 = makeArray(abc.size());
+		ArrayList<Integer> tab2 = GTSolverTest.makeArray(abc.size());
 
-		assertEquals(arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab2, abc, 10, 1))), arrayVarToString(abcSol));
+		assertEquals(GTSolverTest.arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab2, abc, 10, 1))), GTSolverTest.arrayVarToString(abcSol));
 		long end = System.currentTimeMillis();
 		System.out.println("abc par incrementation: "+ String.valueOf(end-start) + "ms");
 
 		start = System.currentTimeMillis();
 		//algo incremental avec repetition
 		GTSolver.setRoot(ABC2);
-		tab2 = makeArray(abc.size());
+		tab2 = GTSolverTest.makeArray(abc.size());
 
-		assertEquals(arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab2, abc, 10, 2))), "A=0 B=2 C=0 ");
+		assertEquals(GTSolverTest.arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab2, abc, 10, 2))), "A=0 B=2 C=0 ");
 		end = System.currentTimeMillis();
 		System.out.println("abc par incrementation avec repetition: "+ String.valueOf(end-start) + "ms");
 
 		start = System.currentTimeMillis();
 		//algo incremental
 		GTSolver.setRoot(sendMoreMoney);
-		ArrayList<Integer> tab3 = makeArray(sendMory.size());
-		assertEquals(arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab3, sendMory, 10, 1))), arrayVarToString(sendMorySol));
+		ArrayList<Integer> tab3 = GTSolverTest.makeArray(sendMory.size());
+		assertEquals(GTSolverTest.arrayVarToString(Objects.requireNonNull(GTSolver.explorationRecursive(tab3, sendMory, 10, 1))), GTSolverTest.arrayVarToString(sendMorySol));
 		end = System.currentTimeMillis();
 		System.out.println("sendmory par incrementation: "+ String.valueOf(end-start) + "ms");
 	}
@@ -265,7 +264,7 @@ public class GTSolverTest {
 			comb.add(i);
 		}
 		CryptaGTSolver GTSolver= new CryptaGTSolver(ABC);
-		assertEquals(arrayVarToString(GTSolver.findSolCrypta(comb, 3, 10, abc, 1)), arrayVarToString(abcSol));
+		assertEquals(GTSolverTest.arrayVarToString(GTSolver.findSolCrypta(comb, 3, 10, abc, 1)), GTSolverTest.arrayVarToString(abcSol));
 		long end = System.currentTimeMillis();
 		System.out.println("abc par heap: "+ String.valueOf(end-start) + "ms");
 
@@ -277,7 +276,7 @@ public class GTSolverTest {
 			comb.add(i);
 		}
 		GTSolver.setRoot(ABC2);
-		assertEquals(arrayVarToString(GTSolver.findSolCrypta(comb, 3, 10, abc, 2)), "A=0 B=1 C=0 ");
+		assertEquals(GTSolverTest.arrayVarToString(GTSolver.findSolCrypta(comb, 3, 10, abc, 2)), "A=0 B=1 C=0 ");
 		end = System.currentTimeMillis();
 		System.out.println("abc par heap avec repetition: "+ String.valueOf(end-start) + "ms");
 
@@ -288,7 +287,7 @@ public class GTSolverTest {
 			comb.add(i);
 		}
 		GTSolver.setRoot(sendMoreMoney);
-		assertEquals(arrayVarToString(GTSolver.findSolCrypta(comb, sendMory.size(), 10, sendMory, 1)), arrayVarToString(sendMorySol));
+		assertEquals(GTSolverTest.arrayVarToString(GTSolver.findSolCrypta(comb, sendMory.size(), 10, sendMory, 1)), GTSolverTest.arrayVarToString(sendMorySol));
 		end = System.currentTimeMillis();
 		System.out.println("sendmory par heap: "+ String.valueOf(end-start) + "ms");
 
@@ -312,6 +311,156 @@ public class GTSolverTest {
 
 
 
+	}
+
+	public static String arrayVarToString(HashMap<Character, GTVariable> map) {
+		StringBuilder sb=new StringBuilder();
+		if(map!=null) {
+			for (GTVariable var : map.values()) {
+				sb.append(var.getName());
+				sb.append("=");
+				sb.append(var.getValue());
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
+
+	public static ArrayList<Integer> nextCombination(ArrayList<Integer> combination, int length, int max) {
+		int last = length - 1;
+	
+		while (last >= 0 && combination.get(last) == max - length + last) {
+			last--;
+		}
+	
+		if (last < 0) return null;
+	
+		combination.set(last, combination.get(last)+1);
+	
+		for (int i = last + 1; i < length; i++) {
+			combination.set(i, combination.get(last) - last + i);
+		}
+	
+		return combination;
+	}
+
+	public static HashMap<Character, GTVariable> findSolCrypta(ArrayList<Integer> comb, int size, int max, HashMap<Character, GTVariable> map, ICryptaNode cryptarithm, int nbRep) throws Exception {
+		if(map.size()>max*nbRep){
+			throw new Exception("to much different letter");
+		}
+		while (comb!=null) {
+			HashMap<Character, GTVariable> m=checkArray(comb, map, cryptarithm, max);
+			if(m!=null){
+	
+				return m;
+			}
+			ArrayList<Integer> perm=new ArrayList<>(comb);
+			m=heapPermutation(perm, size, size, map, cryptarithm, max);
+			if(m!=null){
+				return m;
+			}
+			comb=GTSolverTest.nextCombination(comb, size, max*nbRep);
+		}
+		return null;
+	}
+
+	// Generating permutation using Heap Algorithm
+	public static HashMap<Character, GTVariable> heapPermutation(ArrayList<Integer> a, int size, int n, HashMap<Character, GTVariable> map, ICryptaNode cryptarithm, int max) {
+		// if size becomes 1 then prints the obtained
+		// permutation
+		HashMap<Character, GTVariable> m = null;
+		if (size == 1) {
+			m=checkArray(a, map, cryptarithm, max);
+			return m;
+		}
+	
+		for (int i = 0; i < size; i++) {
+			if(m==null) {
+				m = heapPermutation(a, size - 1, n, map, cryptarithm, max);
+			}
+	
+			// if size is odd, swap 0th i.e (first) and
+			// (size-1)th i.e (last) element
+			if (size % 2 == 1) {
+				int temp = a.get(0);
+				a.set(0, a.get(size - 1));
+				a.set(size - 1, temp);
+			}
+	
+			// If size is even, swap ith
+			// and (size-1)th i.e last element
+			else {
+				int temp = a.get(i);
+				a.set(i, a.get(size - 1));
+				a.set(size - 1, temp);
+			}
+		}
+		return m;
+	}
+
+	public static HashMap<Character, GTVariable> explorationRecursive(ArrayList<Integer> elements,
+														   HashMap<Character, GTVariable> map, ICryptaNode cryptarithm, int max, int nbRep) throws Exception {
+		if(map.size()>max*nbRep){
+			throw new Exception("to much different letter");
+		}
+		for(int i = (int) Math.pow(max,elements.size()); i>0; i--) {
+			elements.set(elements.size()-1, elements.get(elements.size()-1)+1);
+			for(int j=elements.size()-1; j>=0;j--) {
+				if (elements.get(j) == max && j - 1 >=0) {
+					elements.set(j, 0);
+					elements.set(j-1, elements.get(j-1)+1);
+				}
+			}
+			if(elements.get(0)==max){
+				break;
+			}
+			int rep = 0;
+			for (int k=0; k<elements.size(); k++) {
+				for(int l=0; l<elements.size(); l++) {
+					if (elements.get(k).equals(elements.get(l)) && k != l) {
+						rep +=1;
+					}
+				}
+			}
+	
+			if(rep<=nbRep) {
+				HashMap<Character, GTVariable> res = checkArray(elements, map, cryptarithm, max);
+				if (res != null) {
+					return res;
+				}
+			}
+		}
+	
+		return null;
+	}
+
+	public static HashMap<Character, GTVariable> checkArray(ArrayList<Integer> input, HashMap<Character, GTVariable> map, ICryptaNode cryptarithm, int max) {
+		int i=0;
+		for (GTVariable var : map.values()) {
+			if(var.setValue(input.get(i)%max)){
+				i++;
+			}
+			else{
+				return null;
+			}
+		}
+		ICryptaEvaluation chk = new CryptaEvaluation();
+		int v;
+		try {
+			v = chk.evaluate(cryptarithm, new CryptaGTSolution(map), 10);
+		} catch (CryptaEvaluationException e) {
+			e.printStackTrace();
+			v = -1;
+		}
+		return v == 1? map: null;
+	}
+
+	public static ArrayList<Integer> makeArray (int n) {
+		ArrayList<Integer> tab=new ArrayList<>();
+		for (int i=0; i<n; i++){
+			tab.add(i);
+		}
+		return tab;
 	}
 
 
