99a36344d6681b56e208992bdfaef982cf971f13
Arnaud Malapert <arnaud.malapert@univ-cotedazur.fr>
2021-09-12 20:29:42 +0200

    improve the solver CLI close #14 close #16

--- src/main/java/cryptator/Cryptator.java
+++ src/main/java/cryptator/Cryptator.java
@@ -13,6 +13,7 @@ import static cryptator.tree.TreeUtils.writePostorder;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -51,12 +52,12 @@ public class Cryptator {
 		final ICryptaSolver solver = buildSolver(config);
 
 		final CryptaParserWrapper parser = new CryptaParserWrapper();
-
-		final BiConsumer<ICryptaNode, ICryptaSolution> consumer = buildBiConsumer(config);
-
+		
+		int exitStatus = 0;
 		for (String cryptarithm : config.getArguments()) {
-			solve(cryptarithm, parser, solver, config, consumer);
+			exitStatus += solve(cryptarithm, parser, solver, config);
 		}
+		System.exit(exitStatus);
 	}
 
 	static class CryptatorOptionsParser extends OptionsParser<CryptatorConfig> {
@@ -93,17 +94,21 @@ public class Cryptator {
 
 	}
 
-	private static void solve(String cryptarithm, CryptaParserWrapper parser, ICryptaSolver solver , CryptatorConfig config, BiConsumer<ICryptaNode, ICryptaSolution> consumer) {
+	private static int solve(String cryptarithm, CryptaParserWrapper parser, ICryptaSolver solver , CryptatorConfig config) {
 		try {
 			final ICryptaNode node = parseCryptarithm(cryptarithm, parser, LOGGER);
 
 			if(LOGGER.isLoggable(Level.INFO)) {
 				LOGGER.log(Level.INFO, "Cryptarithm features:\n{0}", new CryptaFeatures(node));
 			}
-			// FIXME The checker failure must change the status !
-			// TODO Use also exit code ?
-			final String status = solver.solve(node, config, (s) -> {consumer.accept(node, s);}) ? "OK" : "KO";
+			
+			final AtomicInteger errorCount = new AtomicInteger();
+			final BiConsumer<ICryptaNode, ICryptaSolution> consumer = buildBiConsumer(config, errorCount);
+			final boolean solved = solver.solve(node, config, (s) -> {consumer.accept(node, s);}) ;
+			final String status = errorCount.get() > 0 ? "ERROR" : (solved ? "OK" : "KO");
 			LOGGER.log(Level.INFO, "Solve cryptarithm {0} [{1}]", new Object[] {cryptarithm, status});
+			return errorCount.get();
+		
 		} catch (CryptaParserException e) {
 			LOGGER.log(Level.SEVERE, "Parse cryptarithm " + cryptarithm + " [FAIL]", e);
 		} catch (CryptaModelException e) {
@@ -111,37 +116,47 @@ public class Cryptator {
 		} catch (CryptaSolverException e) {
 			LOGGER.log(Level.SEVERE, "Solve cryptarithm [FAIL]", e);
 		}
+		return 1;
 	}
 
 	private static class DefaultConsumer implements BiConsumer<ICryptaNode, ICryptaSolution> {
 
 		private int solutionCount = 0;
+			
 		@Override
 		public void accept(ICryptaNode n, ICryptaSolution s) {
 			solutionCount++;
-			LOGGER.log(Level.INFO, "Solution #{0}.\n{1}", new Object[] {solutionCount, s});
+			LOGGER.log(Level.INFO, "Find cryptarithm solution #{0} [OK]\n{1}", new Object[] {solutionCount, s});
 		}
 	}
 
+
 	private static class CheckConsumer implements BiConsumer<ICryptaNode, ICryptaSolution> {
+		
+		private final AtomicInteger error;
+		
+		private final int base;
 
-		public final int base;
+		private final ICryptaEvaluation eval = new CryptaEvaluation();
 
-		public final ICryptaEvaluation eval = new CryptaEvaluation();
-
-		public CheckConsumer(int base) {
+		public CheckConsumer(int base, AtomicInteger error) {
 			super();
 			this.base = base;
+			this.error = error;
 		}
 
 		@Override
 		public void accept(ICryptaNode n, ICryptaSolution s) {
 			try {
-				if(eval.evaluate(n, s, base) != 0) LOGGER.config("Eval cryptarithm solution [OK]"); 
-				else  LOGGER.warning("Eval cryptarithm solution [KO]"); 
+				if(eval.evaluate(n, s, base) != 0) {
+					LOGGER.info("Eval cryptarithm solution [OK]"); 
+					return;
+				}
+				else LOGGER.warning("Eval cryptarithm solution [KO]"); 
 			} catch (CryptaEvaluationException e) {
 				LOGGER.log(Level.WARNING, "Eval cryptarithm solution [FAIL]", e);
-			}		
+			}
+			error.incrementAndGet();
 		}
 	}
 
@@ -150,10 +165,10 @@ public class Cryptator {
 		@Override
 		public void accept(ICryptaNode n, ICryptaSolution s) {
 			try {
-				Graph graph = GraphvizExport.exportToGraphviz(n, s);
-				File file = File.createTempFile("cryptarithm-", ".svg");
+				final Graph graph = GraphvizExport.exportToGraphviz(n, s);
+				final File file = File.createTempFile("cryptarithm-", ".svg");
 				Graphviz.fromGraph(graph).width(800).render(Format.SVG).toFile(file);
-				LOGGER.log(Level.INFO, "Export cryptarithm solution to {0} [OK]", file);
+				LOGGER.log(Level.INFO, "Export cryptarithm solution [OK]\n{0}", file);
 			} catch (IOException e) {
 				LOGGER.log(Level.SEVERE, "Export cryptarithm solution [FAIL]", e);
 
@@ -161,11 +176,22 @@ public class Cryptator {
 		}
 	}
 
+	private static BiConsumer<ICryptaNode, ICryptaSolution> buildBiConsumer(final CryptatorConfig config, AtomicInteger errorCount) {
+		BiConsumer<ICryptaNode, ICryptaSolution> consumer = new DefaultConsumer();
+		if(config.isCheckSolution()) {
+			consumer = consumer.andThen(new CheckConsumer(config.getArithmeticBase(), errorCount));
+		}
+		if(config.isExportGraphiz()) {
+			consumer = consumer.andThen(new GraphvizConsumer());
+		}
+		return consumer;
+	}
+	
 	public static BiConsumer<ICryptaNode, ICryptaSolution> buildBiConsumer(final CryptatorConfig config) {
 		BiConsumer<ICryptaNode, ICryptaSolution> consumer = new DefaultConsumer();
-
+		AtomicInteger errorCount = new AtomicInteger();
 		if(config.isCheckSolution()) {
-			consumer = consumer.andThen( new CheckConsumer(config.getArithmeticBase()));
+			consumer = consumer.andThen(new CheckConsumer(config.getArithmeticBase(), errorCount));
 		}
 		if(config.isExportGraphiz()) {
 			consumer = consumer.andThen(new GraphvizConsumer());
