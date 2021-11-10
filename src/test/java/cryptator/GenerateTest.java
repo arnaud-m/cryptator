e8ae2c27abe231b6c486cce62464c7a749a6cc85
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2021-11-10 15:39:24 +0100

    add generate tests

---
+++ src/test/java/cryptator/GenerateTest.java
@@ -0,0 +1,68 @@
+package cryptator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cryptator.cmd.CryptaBiConsumer;
+import cryptator.cmd.WordArray;
+import cryptator.config.CryptagenConfig;
+import cryptator.gen.CryptaListGenerator;
+import cryptator.solver.CryptaModelException;
+
+public class GenerateTest {
+
+	@BeforeClass
+	public static void configureTestLoggers() {
+		JULogUtil.configureTestLoggers();
+	}
+	
+	public long testGenerate(WordArray wordArray) throws CryptaModelException {
+		final CryptagenConfig config = new CryptagenConfig();
+		final CryptaListGenerator gen = new CryptaListGenerator(wordArray, config, Cryptagen.LOGGER);
+		CryptaBiConsumer cons = new CryptaBiConsumer(Cryptagen.LOGGER);
+		cons.withSolutionCheck(config.getArithmeticBase());
+		gen.generate(cons);
+		assertEquals(0, cons.getErrorCount());
+		return cons.getSolutionCount();
+	}
+	
+	public long testGenerate(String rightMember, String... words) throws CryptaModelException {
+		return testGenerate(new WordArray(Arrays.asList(words), rightMember));
+	}
+	
+	@Test
+	public void testSendMoreMoney() throws CryptaModelException {
+	assertEquals(1, testGenerate(null, "send", "more", "money"));
+	}
+	
+	@Test
+	public void testPlanets1() throws CryptaModelException {
+		assertEquals(2, testGenerate(null, "venus", "earth", "uranus", "saturn"));
+	}
+	
+	@Test
+	public void testPlanets2() throws CryptaModelException {
+		assertEquals(1, testGenerate("planets", "venus", "earth", "uranus", "saturn"));
+	}
+	
+	@Test
+	public void testDoublyTrue1() throws CryptaModelException {
+		assertEquals(0, testGenerate(new WordArray("FR", "fr", 0, 10)));
+	}
+	
+	@Test
+	public void testDoublyTrue2() throws CryptaModelException {
+		assertEquals(1, testGenerate(new WordArray("FR", "fr", 30, 30)));
+	}
+	
+	@Test
+	public void testDoublyTrue3() throws CryptaModelException {
+		assertEquals(3, testGenerate(new WordArray("IT", "it", 20, 30)));
+	}
+	
+	
+}
