460f94bfb5c376894448cb81ba0d6161dfa422c8
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2021-11-11 17:18:41 +0100

    add tests for choco expression problems

--- src/test/java/cryptator/Failing.java
+++ src/test/java/cryptator/Failing.java
@@ -10,6 +10,8 @@ package cryptator;
 
 import static org.junit.Assert.assertEquals;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.IntVar;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -21,7 +23,7 @@ import cryptator.solver.CryptaSolverException;
 
 public class Failing {
 
-	public final CryptaSolvingTester t = new CryptaSolvingTester(new CryptaSolver(true));
+	public final CryptaSolvingTester t = new CryptaSolvingTester(new CryptaSolver(false));
 
 	public Failing() {}
 
@@ -64,12 +66,32 @@ public class Failing {
 	}
 
 	@Test
-	//FIXME confusion between division operator (integer/real)
+	@Ignore 
 	public void testMrazik() throws CryptaParserException, CryptaModelException, CryptaSolverException {
+		// Take around 1 minute
 		assertEquals("Div and Mult",
 				t.testSAT("A * (4*TH + OF*JULY) = HAPPY"),
 				t.testSAT("A = HAPPY / (4*TH + OF*JULY)")
 				);
 	}	
+	
+	@Test
+	public void testExpression1() {
+		Model m = new Model();
+		IntVar x = m.intVar(IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND);
+		IntVar y = m.intVar(IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND);
+		IntVar xy = x.mul(y).mul(101).mul(101).intVar();
+		
+	}
+	
+	@Test
+	public void testExpression2() {
+		Model m = new Model();
+		IntVar x = m.intVar(0, 10);
+		IntVar y = m.intVar(0, 10);
+		IntVar z = m.intVar(0, 100);
+		
+		x.pow(y).eq(z).post();
+	}
 
 }
