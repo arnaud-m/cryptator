c53ded6e646b6c970c840021155aad8fa0e5c38f
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2021-12-10 18:23:02 +0100

    add DOREMUS words lists  #23

---
+++ src/main/shell/wget-doremus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## Download files if necessary
+FILES="artist.emb.l genre.emb.l mop.emb.l"
+for FILE in $FILES ; do
+    echo $FILE
+    if [ ! -f $FILE ] ; then
+        echo "WGET"
+        wget "https://raw.githubusercontent.com/DOREMUS-ANR/music-embeddings/master/$FILE"
+    fi
+done
+
+## Remove leading URL of artist file
+sed -i -e "s/^[^[:space:]]*//" artist.emb.l
+
+## Clean downloaded files
+./clean-words-list.sh $FILES
