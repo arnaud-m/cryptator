28d3686ecc6212b0d869f4e2e94eeae2fdb60328
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-11-02 22:35:53 +0100

    refactor, document,  and test conversion of numbers to words #34

--- src/main/java/cryptator/gen/TransformWord.java
+++ src/main/java/cryptator/gen/TransformWord.java
@@ -14,33 +14,93 @@ import com.ibm.icu.lang.UCharacter;
 import com.ibm.icu.text.Normalizer2;
 import com.ibm.icu.text.RuleBasedNumberFormat;
 
+/**
+ * The Class TransformWord provides helper static functions that wrap calls to the icu and java.string libraries
+ */
 public final class TransformWord {
-	
+
+	/**
+	 * Instantiates a new transform word is private.
+	 */
 	private TransformWord() {
 		super();
 	}
 
+	/**
+	 * Translate an integer into words (decimal integer) in any language.
+	 *
+	 * @param ctryCd the country code of the java.util.Locale
+	 * @param lang the language of the java.util.Locale
+	 * @param value the integer value
+	 * @return the integer converted to words
+	 */
 	public static String translate(String ctryCd, String lang, int value) {
 		final Locale locale = new Locale(lang, ctryCd);
 		final RuleBasedNumberFormat rule = new RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT);
 		return rule.format(value);
 	}
-	
+
+	/**
+	 * Translate and normalize an integer into words (decimal integer) in any language.
+	 *
+	 * @param ctryCd the country code of the java.util.Locale
+	 * @param lang the language of the java.util.Locale
+	 * @param value the integer value
+	 * @return the integer converted to normalized words
+	 */
+	public static String translateAndNormalize(String ctryCd, String lang, int value) {
+		final String translated = translate(ctryCd, lang, value);
+		return toLowerCase(removeWhitespaces(removeDashes(stripAccents(translated))));
+	}
+
+	/**
+	 * Strip accents of the input.
+	 *
+	 * @param input the input
+	 * @return the input without accent
+	 */
 	public static String stripAccents(String input) {
 		final String normalized = Normalizer2.getNFDInstance().normalize(input);
 		return normalized.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
 	}
-	
+
+	/**
+	 * Removes the whitespaces of the input.
+	 *
+	 * @param input the input
+	 * @return the string without whitespaces
+	 */
 	public static String removeWhitespaces(String input) {
 		return input.replaceAll("\\s+", "");
 	}
-	
+
+	/**
+	 * Removes the dashes of the input.
+	 *
+	 * @param input the input
+	 * @return the input without dashes
+	 */
 	public static String removeDashes(String input) {
 		return input.replace("-", "");
 	}
-	
+
+	/**
+	 * Convert the input to lower case.
+	 *
+	 * @param input the input
+	 * @return the input converted to lower case
+	 */
 	public static String toLowerCase(String input) {
 		return UCharacter.toLowerCase(input);
 	}
 
+	/**
+	 * Convert the input to upper case.
+	 *
+	 * @param input the input
+	 * @return the input converted to upper case
+	 */
+	public static String toUpperCase(String input) {
+		return UCharacter.toUpperCase(input);
+	}
 }
