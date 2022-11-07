ab5d7643f3c03b56a6e1d74fb7eab9561a9aa91f
Davide Fissore <fisso9900@gmail.com>
2022-11-07 17:42:14 +0100

    #31 reindent CryptaNode with Eclipse formatter

--- src/main/java/cryptator/tree/CryptaNode.java
+++ src/main/java/cryptator/tree/CryptaNode.java
@@ -13,60 +13,60 @@ import cryptator.specs.ICryptaNode;
 
 public class CryptaNode implements ICryptaNode {
 
-    private final CryptaOperator operator;
+	private final CryptaOperator operator;
 
-    private final ICryptaNode leftChild;
+	private final ICryptaNode leftChild;
 
-    private final ICryptaNode rightChild;
+	private final ICryptaNode rightChild;
 
-    public CryptaNode(String operator, ICryptaNode leftChild, ICryptaNode rightChild) {
-        this(CryptaOperator.valueOfToken(operator), leftChild, rightChild);
-    }
+	public CryptaNode(String operator, ICryptaNode leftChild, ICryptaNode rightChild) {
+		this(CryptaOperator.valueOfToken(operator), leftChild, rightChild);
+	}
 
-    public CryptaNode(CryptaOperator operator, ICryptaNode leftChild, ICryptaNode rightChild) {
-        this.operator = operator;
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
-    }
+	public CryptaNode(CryptaOperator operator, ICryptaNode leftChild, ICryptaNode rightChild) {
+		this.operator = operator;
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
 
-    @Override
-    public CryptaOperator getOperator() {
-        return operator;
-    }
+	@Override
+	public CryptaOperator getOperator() {
+		return operator;
+	}
 
-    @Override
-    public char[] getWord() {
-        return operator.getToken().toCharArray();
-    }
+	@Override
+	public char[] getWord() {
+		return operator.getToken().toCharArray();
+	}
 
-    @Override
-    public ICryptaNode getLeftChild() {
-        return leftChild;
-    }
+	@Override
+	public ICryptaNode getLeftChild() {
+		return leftChild;
+	}
 
-    @Override
-    public ICryptaNode getRightChild() {
-        return rightChild;
-    }
+	@Override
+	public ICryptaNode getRightChild() {
+		return rightChild;
+	}
 
-    @Override
-    public boolean isLeaf() {
-        return false;
-    }
+	@Override
+	public boolean isLeaf() {
+		return false;
+	}
 
-    @Override
-    public boolean isConstant() {
-        return false;
-    }
+	@Override
+	public boolean isConstant() {
+		return false;
+	}
 
-    @Override
-    public boolean isInternalNode() {
-        return true;
-    }
+	@Override
+	public boolean isInternalNode() {
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return operator.getToken();
-    }
+	@Override
+	public String toString() {
+		return operator.getToken();
+	}
 
 }
