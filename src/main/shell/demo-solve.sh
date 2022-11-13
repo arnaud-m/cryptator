8a7d0b4f4fec7683252dfa8bda1bdd8dc0e6217a
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-11-13 15:52:50 +0100

    add the license header in scripts #43

--- src/main/shell/demo-solve.sh
+++ src/main/shell/demo-solve.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+# This file is part of cryptator, https://github.com/arnaud-m/cryptator
+#
+# Copyright (c) 2022, Université Côte d'Azur. All rights reserved.
+#
+# Licensed under the BSD 3-clause license.
+# See LICENSE file in the project root for full license information.
+#
+
 JAR="../../../target/cryptator-*-with-dependencies.jar"
 CMD="java -cp $JAR cryptator.Cryptator -c TRUE -g TRUE"
 
