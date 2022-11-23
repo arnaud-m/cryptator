b4b6571d8622740686809d132504077c1a34a73b
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-11-23 21:36:31 +0100

    define dimacs logging for choco solver #63

--- src/main/java/cryptator/solver/CryptaSolver.java
+++ src/main/java/cryptator/solver/CryptaSolver.java
@@ -8,10 +8,13 @@
  */
 package cryptator.solver;
 
+import java.util.Formatter;
+import java.util.Locale;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
 
@@ -80,7 +83,10 @@ public final class CryptaSolver implements ICryptaSolver {
     public boolean solve(final ICryptaNode cryptarithm, final CryptaConfig config,
             final Consumer<ICryptaSolution> solutionConsumer) throws CryptaModelException {
         final CryptaModel m = modeler.model(cryptarithm, config);
-        LOGGER.log(Level.CONFIG, "Display model{0}", m.getModel());
+
+        LOGGER.log(Level.INFO, "Model diagnostics:{0}", toDimacs(m.getModel()));
+        LOGGER.log(Level.CONFIG, "Printed model:{0}", m.getModel());
+
         final Solver s = m.getModel().getSolver();
         if (timeLimit > 0) {
             s.limitTime(timeLimit * MS); // in ms
@@ -99,8 +105,35 @@ public final class CryptaSolver implements ICryptaSolver {
                 solutionCount++;
             }
         }
-        LOGGER.log(Level.INFO, "{0}", s.getMeasures());
+        LOGGER.log(Level.INFO, "Solver diagnostics:{0}", toDimacs(s));
         return solutionCount > 0;
+    }
+
+    public static String toDimacs(final Solver s) {
+        final StringBuilder b = new StringBuilder(256);
+        Formatter fmt = new Formatter(b, Locale.US);
+        fmt.format("%ns %s", s.getSearchState());
+        if (s.hasObjective()) {
+            fmt.format("%no %3.f", s.getBoundsManager().getBestSolutionValue());
+        }
+        fmt.format(
+                "%nd NBSOLS %d%nd TIME %.3f%nd NODES %d%nd BACKTRACKS %d%nd BACKJUMPS %d%nd FAILURES %d%nd RESTARTS %d",
+                s.getSolutionCount(), s.getTimeCount(), s.getNodeCount(), s.getBackTrackCount(), s.getBackjumpCount(),
+                s.getFailCount(), s.getRestartCount());
+        fmt.close();
+        return b.toString();
+    }
+
+    public static String toDimacs(Model model) {
+        final StringBuilder b = new StringBuilder();
+        Formatter fmt = new Formatter(b, Locale.US);
+        fmt.format("%nc MODEL_NAME %s", model.getName());
+        fmt.format("%nd VARIABLES %d", model.getNbVars());
+        fmt.format("%nd CONSTRAINTS %d", model.getNbCstrs());
+        fmt.format("%nd BOOL_VARS %d", model.getNbBoolVar());
+        fmt.format("%nd INT_VARS %d", model.getNbIntVar(false));
+        fmt.close();
+        return b.toString();
     }
 
 }
