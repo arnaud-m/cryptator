f47da77ab9855d9e04cf3dd0bf021c111f763833
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-11-28 12:47:19 +0100

    create class for logging choco objects #63

--- src/main/java/cryptator/solver/CryptaSolver.java
+++ src/main/java/cryptator/solver/CryptaSolver.java
@@ -8,16 +8,12 @@
  */
 package cryptator.solver;
 
-import java.util.Formatter;
-import java.util.Locale;
 import java.util.function.Consumer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.chocosolver.solver.Model;
-import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
 
+import cryptator.choco.ChocoLogger;
 import cryptator.config.CryptaConfig;
 import cryptator.specs.ICryptaModeler;
 import cryptator.specs.ICryptaNode;
@@ -29,6 +25,8 @@ public final class CryptaSolver implements ICryptaSolver {
     private static final int MS = 1000;
 
     public static final Logger LOGGER = Logger.getLogger(CryptaSolver.class.getName());
+
+    private static final ChocoLogger CLOG = new ChocoLogger(LOGGER);
 
     private ICryptaModeler modeler;
 
@@ -71,21 +69,12 @@ public final class CryptaSolver implements ICryptaSolver {
         modeler = new CryptaModeler();
     }
 
-    private void logOnSolution(final CryptaModel m) {
-        if (LOGGER.isLoggable(Level.CONFIG)) {
-            final Solution sol = new Solution(m.getModel());
-            sol.record();
-            LOGGER.log(Level.CONFIG, "Display internal solver solution.\n{0}", sol);
-        }
-    }
-
     @Override
     public boolean solve(final ICryptaNode cryptarithm, final CryptaConfig config,
             final Consumer<ICryptaSolution> solutionConsumer) throws CryptaModelException {
         final CryptaModel m = modeler.model(cryptarithm, config);
 
-        LOGGER.log(Level.INFO, "Model diagnostics:{0}", toDimacs(m.getModel()));
-        LOGGER.log(Level.CONFIG, "Printed model:{0}", m.getModel());
+        CLOG.logOnModel(m.getModel());
 
         final Solver s = m.getModel().getSolver();
         if (timeLimit > 0) {
@@ -94,46 +83,19 @@ public final class CryptaSolver implements ICryptaSolver {
         int solutionCount = 0;
         if (solutionLimit > 0) {
             while ((solutionCount < solutionLimit) && s.solve()) {
-                logOnSolution(m);
+                CLOG.logOnSolution(m.getModel());
                 solutionConsumer.accept(m.recordSolution());
                 solutionCount++;
             }
         } else {
             while (s.solve()) {
-                logOnSolution(m);
+                CLOG.logOnSolution(m.getModel());
                 solutionConsumer.accept(m.recordSolution());
                 solutionCount++;
             }
         }
-        LOGGER.log(Level.INFO, "Solver diagnostics:{0}", toDimacs(s));
+        CLOG.logOnSolver(m.getModel());
         return solutionCount > 0;
-    }
-
-    public static String toDimacs(final Solver s) {
-        final StringBuilder b = new StringBuilder(256);
-        Formatter fmt = new Formatter(b, Locale.US);
-        fmt.format("%ns %s", s.getSearchState());
-        if (s.hasObjective()) {
-            fmt.format("%no %3.f", s.getBoundsManager().getBestSolutionValue());
-        }
-        fmt.format(
-                "%nd NBSOLS %d%nd TIME %.3f%nd NODES %d%nd BACKTRACKS %d%nd BACKJUMPS %d%nd FAILURES %d%nd RESTARTS %d",
-                s.getSolutionCount(), s.getTimeCount(), s.getNodeCount(), s.getBackTrackCount(), s.getBackjumpCount(),
-                s.getFailCount(), s.getRestartCount());
-        fmt.close();
-        return b.toString();
-    }
-
-    public static String toDimacs(Model model) {
-        final StringBuilder b = new StringBuilder();
-        Formatter fmt = new Formatter(b, Locale.US);
-        fmt.format("%nc MODEL_NAME %s", model.getName());
-        fmt.format("%nd VARIABLES %d", model.getNbVars());
-        fmt.format("%nd CONSTRAINTS %d", model.getNbCstrs());
-        fmt.format("%nd BOOL_VARS %d", model.getNbBoolVar());
-        fmt.format("%nd INT_VARS %d", model.getNbIntVar(false));
-        fmt.close();
-        return b.toString();
     }
 
 }
