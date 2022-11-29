25a5c304035f344a9dcaeb33638aeb6c8a77510e
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2022-11-29 16:06:36 +0100

    create enum of verbosity levels #70

---
+++ src/main/java/cryptator/cmd/Verbosity.java
@@ -0,0 +1,33 @@
+/**
+ * This file is part of cryptator, https://github.com/arnaud-m/cryptator
+ *
+ * Copyright (c) 2022, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator.cmd;
+
+import cryptator.specs.ICryptaLogManager;
+
+public enum Verbosity {
+    SILENT, QUIET, NORMAL, VERBOSE, VERY_VERBOSE, DEBUG;
+
+    public static void setVerbosity(Verbosity verbosity, ICryptaLogManager manager) {
+        if (verbosity != null && manager != null) {
+            if (verbosity.equals(SILENT)) {
+                manager.setSilent();
+            } else if (verbosity.equals(QUIET)) {
+                manager.setQuiet();
+            } else if (verbosity.equals(NORMAL)) {
+                manager.setNormal();
+            } else if (verbosity.equals(VERBOSE)) {
+                manager.setVerbose();
+            } else if (verbosity.equals(VERY_VERBOSE)) {
+                manager.setVeryVerbose();
+            } else if (verbosity.equals(DEBUG)) {
+                manager.setDebug();
+            }
+        }
+    }
+}
