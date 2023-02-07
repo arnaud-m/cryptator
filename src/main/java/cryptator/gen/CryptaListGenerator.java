40f01aecf59be568be3a0eff7e18f77183eb7ebe
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2023-02-07 18:01:19 +0100

    use abstract methods of gen. models #117

--- src/main/java/cryptator/gen/CryptaListGenerator.java
+++ src/main/java/cryptator/gen/CryptaListGenerator.java
@@ -62,46 +62,25 @@ public class CryptaListGenerator implements ICryptaGenerator {
         return errorCount.intValue();
     }
 
-    ICryptaGenSolver buildAdditionSolver() {
-        final CryptaGenModel gen = new CryptaGenModel(words.getWords(), config.isLightModel());
-        gen.buildModel();
-        gen.postWordCountConstraints(Math.max(config.getMinLeftOperands(), 2) + 1, config.getMaxLeftOperands() + 1);
-        gen.postMaxSymbolCountConstraint(config.getArithmeticBase());
-        if (!config.isLightPropagation()) {
-            gen.postMinLeftCountConstraints(config.getArithmeticBase());
+    private AbstractCryptaListModel createGenModel() {
+        if (config.getGridSize() > 0) {
+            return new CryptaGenCrossword(config.getGridSize(), words.getWords(), config.isLightModel());
+        } else if (config.isMultModel()) {
+            return new CryptaGenMult(words.getWords());
+        } else if (config.isLongMultModel()) {
+            return new CryptaGenLongMult(words.getWords());
+        } else {
+            return new CryptaGenModel(words.getWords(), config.isLightModel());
         }
-        if (words.hasRightMember()) {
-            gen.postFixedRightMemberConstraint();
-        }
-        if (words.isDoublyTrue()) {
-            gen.postDoublyTrueConstraint(words.getLB());
-        }
-        return gen;
     }
 
-    ICryptaGenSolver buildLongMultiplicationSolver() {
-        final CryptaGenLongMult gen = new CryptaGenLongMult(words.getWords());
+    private ICryptaGenSolver buildGenSolver() {
+        final AbstractCryptaListModel gen = createGenModel();
         gen.buildModel();
         gen.postWordCountConstraints(Math.max(config.getMinLeftOperands(), 2) + 1, config.getMaxLeftOperands() + 1);
         gen.postMaxSymbolCountConstraint(config.getArithmeticBase());
         gen.postPrecisionConstraints(config.getArithmeticBase());
-//        if (!config.isLightPropagation()) {
-//            gen.postMinLeftCountConstraints(config.getArithmeticBase());
-//        }
-        if (words.hasRightMember()) {
-            gen.postFixedRightMemberConstraint();
-        }
-//        if (words.isDoublyTrue()) {
-//            gen.postDoublyTrueConstraint(words.getLB());
-//        }
-        return gen;
-    }
 
-    ICryptaGenSolver buildMultiplicationSolver() {
-        final CryptaGenMult gen = new CryptaGenMult(words.getWords());
-        gen.buildModel();
-        gen.postWordCountConstraints(Math.max(config.getMinLeftOperands(), 2) + 1, config.getMaxLeftOperands() + 1);
-        gen.postMaxSymbolCountConstraint(config.getArithmeticBase());
         if (!config.isLightPropagation()) {
             gen.postMinLeftCountConstraints(config.getArithmeticBase());
         }
@@ -112,30 +91,6 @@ public class CryptaListGenerator implements ICryptaGenerator {
             gen.postDoublyTrueConstraint(words.getLB());
         }
         return gen;
-    }
-
-    ICryptaGenSolver buildCrosswordSolver() {
-        final CryptaGenCrossword gen = new CryptaGenCrossword(config.getGridSize(), words.getWords(),
-                config.isLightModel());
-        gen.buildModel();
-        gen.postWordCountConstraints(Math.max(config.getMinLeftOperands(), 2) + 1, config.getMaxLeftOperands() + 1);
-        gen.postMaxSymbolCountConstraint(config.getArithmeticBase());
-        if (!config.isLightPropagation()) {
-            gen.postHeavyConstraints(config.getArithmeticBase());
-        }
-        return gen;
-    }
-
-    private ICryptaGenSolver buildGenSolver() {
-        if (config.getGridSize() > 0) {
-            return buildCrosswordSolver();
-        } else if (config.isMultModel()) {
-            return buildMultiplicationSolver();
-        } else if (config.isLongMultModel()) {
-            return buildLongMultiplicationSolver();
-        } else {
-            return buildAdditionSolver();
-        }
     }
 
     private Consumer<ICryptaNode> buildConsumer(final IChocoModel gen,
